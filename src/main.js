39ec999894991899e6de761f3bde450085ef65cf
Yaya-Cout <yaya.cout@free.fr>
2022-07-31 17:53:02 +0200

    [src/router] Add router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
+import router from './router'
 
-createApp(App).use(i18n).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
