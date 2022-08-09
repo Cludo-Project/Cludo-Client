3e37feea65d4e884053a4450c420094a356de9f5
Yaya-Cout <yaya.cout@free.fr>
2022-08-09 11:15:40 +0200

    [src/app] Move navbar to component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,6 @@
 <template>
   <div>
-    <nav>
-      <router-link to="/">
-        <img
-          alt="Logo"
-          src="@/assets/logo.png"
-          class="logo"
-        >
-      </router-link>
-      <router-link to="/">
-        {{ t('home.title') }}
-      </router-link> |
-      <router-link to="/games">
-        {{ t('games.title') }}
-      </router-link> |
-      <router-link to="/about">
-        {{ t('about.title') }}
-      </router-link>
-      <LocaleSwitcher />
-    </nav>
+    <NavbarComponent />
     <router-view />
   </div>
 </template>
@@ -26,11 +8,11 @@
 <script>
 import { defineComponent } from 'vue'
 import { useI18n } from 'vue-i18n'
-import LocaleSwitcher from '@/components/LocaleSwitcher.vue'
+import NavbarComponent from './components/NavbarComponent.vue'
 export default defineComponent({
   components: {
-    LocaleSwitcher
-  },
+    NavbarComponent
+},
   setup() {
     const { t } = useI18n({
       inheritLocale: true,
@@ -55,31 +37,5 @@ export default defineComponent({
 
 a {
   text-decoration: none;
-}
-
-nav {
-  /* Align left */
-  text-align: left;
-  padding: 30px;
-  font-size: 1.2rem;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.logo {
-  border-radius: 100%;
-  /* Use the same width than the top bar */
-  width: 40px;
-  height: 40px;
-  margin-right: 20px;
-  /* Center vertically in the top bar */
-  vertical-align: middle;
 }
 </style>
---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,68 @@
+<template>
+  <nav>
+    <router-link to="/">
+      <img
+        alt="Logo"
+        src="@/assets/logo.svg"
+        class="logo"
+      >
+    </router-link>
+    <router-link to="/">
+      {{ t('home.title') }}
+    </router-link> |
+    <router-link to="/games">
+      {{ t('games.title') }}
+    </router-link> |
+    <router-link to="/about">
+      {{ t('about.title') }}
+    </router-link>
+    <LocaleSwitcher />
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
+import LocaleSwitcher from './LocaleSwitcher.vue'
+
+export default defineComponent({
+    name: "NavbarComponent",
+    components: { LocaleSwitcher },
+    setup() {
+        const { t } = useI18n({
+            inheritLocale: true,
+            useScope: "global"
+        });
+        return { t };
+    }
+})
+</script>
+
+<style scoped>
+
+nav {
+  /* Align left */
+  text-align: left;
+  padding: 30px;
+  font-size: 1.2rem;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.logo {
+  border-radius: 100%;
+  /* Use the same width than the top bar */
+  width: 40px;
+  height: 40px;
+  margin-right: 20px;
+  /* Center vertically in the top bar */
+  vertical-align: middle;
+}
+</style>
